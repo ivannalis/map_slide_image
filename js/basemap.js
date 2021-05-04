1753c17f5ff9aee919de7ba530df686afe75b448
ivannalis <71641625+ivannalis@users.noreply.github.com>
2021-05-05 02:02:35 +0700

    First

---
+++ js/basemap.js
@@ -0,0 +1,37 @@
+/* -------------------------------------------------------------------------- */
+/*                                load basemap                                */
+/* -------------------------------------------------------------------------- */
+
+const map = L.map('map').setView([
+    -0.040, 109.356
+], 14.2);
+let layer = L.esri.basemapLayer('DarkGray').addTo(map);
+let layerLabels;
+
+function setBasemap(basemap) {
+    if (layer) {
+        map.removeLayer(layer);
+    }
+    layer = L.esri.basemapLayer(basemap);
+    map.addLayer(layer);
+    if (layerLabels) {
+        map.removeLayer(layerLabels);
+    }
+
+    if (basemap === 'ShadedRelief' || basemap === 'Oceans' || basemap === 'Gray' || basemap === 'DarkGray' ||
+        basemap === 'Imagery' || basemap === 'Terrain') {
+
+        layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+        map.addLayer(layerLabels);
+    }
+}
+
+let basemaps = document.getElementById('basemaps');
+
+basemaps.addEventListener('change', function () {
+    setBasemap(basemaps.value);
+});
+
+/* -------------------------------------------------------------------------- */
+/*                               GEOJSON Section                              */
+/* -------------------------------------------------------------------------- */
---
+++ js/loadmap.js
@@ -0,0 +1,62 @@
+const GEOJSON_PATH = 'assets/geojson/location.geojson'
+const IMG_FOLDER = "assets/images/"
+
+let serviceIcon = L.icon({
+    iconUrl: 'assets/geojson/placeholder.png',
+    iconSize: [25, 25],
+    shadowSize: [0, 0],
+    iconAnchor: [10, 10],
+    shadowAnchor: [0, 0],
+    popupAnchor: [-10, -10]
+});
+
+let file = $.ajax({
+    url: GEOJSON_PATH,
+    dataType: "json",
+    success: console.log("County data successfully loaded."),
+    error: function (xhr) {
+        alert(xhr.statusText)
+    }
+})
+
+$.when(file).done(function () {
+    let layer = file.responseJSON
+    function onEachFeature(feature, layer) {
+        layer.on('click', function (e) {
+            let container = document.getElementById('abscard')
+            container.innerHTML =
+             `
+            <div id="card-row" class="row">
+                <div class="col-sm">
+                   <p><i class="bi bi-geo-alt-fill"></i></p>
+                   <h3 style="font-weight:bold;">
+                    ${feature.properties.Address}
+                    </h3>
+                </div>
+                <div class="col-sm">
+                    <p><i class="bi bi-compass"></i></p>
+                    <h3 id="coor" style="font-weight:bold;">
+                    ${feature.geometry.coordinates} 
+                    </h3>
+                </div>
+            </div>
+            <img class="responsive-iframe" frameborder="0" scrolling="no" src="${IMG_FOLDER + feature.properties.img_loc}"></img>
+            <p class="caption">Description: ${feature.properties.description}</p>
+            `
+        });
+    }
+
+
+
+    let layers = L.geoJSON(layer, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            let marker = L.marker(latlng, {
+                icon: serviceIcon
+            });
+            return marker;
+        }
+    }).addTo(map)
+
+    
+})
